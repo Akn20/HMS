71ff814f85017826eed6040ddb0ea84f28f61772
BlxrryFxce17 <akashvmsn@gmail.com>
2026-02-16 15:46:48 +0530

    Add financial year migrations, controller and views

---
+++ app/Models/FinancialYear.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class FinancialYear extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'start_date',
+        'end_date',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+        'is_active' => 'boolean',
+    ];
+
+    public function scopeForDate($query, $date)
+    {
+        return $query->where('start_date', '<=', $date)
+            ->where('end_date', '>=', $date);
+    }
+}
